e9f40c95f7c73e22f34ccec89fc85519bf365ae8
gaidsalah311-alt <gaidsalah311@gmail.com>
2026-08-09 10:50:09 +0100

    feat: route real book creation page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import Browse from "@/pages/Browse";
 import Profile from "@/pages/Profile";
 import BookDetail from "@/pages/BookDetail";
 import SearchBooks from "@/pages/SearchBooks";
+import AddBook from "@/pages/AddBook";
 import AuthorProfile from "@/pages/AuthorProfile";
 import PublisherProfile from "@/pages/PublisherProfile";
 import BookstoreProfile from "@/pages/BookstoreProfile";
@@ -23,21 +24,11 @@ import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 
-function Router() {
-  return <Switch>
-    <Route path="/" component={Home} /><Route path="/browse" component={Browse} /><Route path="/search" component={SearchBooks} />
-    <Route path="/books/:slug" component={BookDetail} /><Route path="/books/:bookId/reviews" component={Reviews} />
-    <Route path="/authors/:userId" component={AuthorProfile} /><Route path="/publishers/:publisherId" component={PublisherProfile} /><Route path="/bookstores/:bookstoreId" component={BookstoreProfile} />
-    <Route path="/profile" component={Profile} /><Route path="/admin" component={AdminDashboard} /><Route path="/admin/reports" component={ReportsManagement} /><Route path="/dashboard" component={AdminDashboard} />
-    <Route path="/orders" component={Orders} /><Route path="/advertisements" component={AdvertisementManagement} /><Route path="/featured-listings" component={FeaturedListingManagement} />
-    <Route path="/subscriptions" component={Subscriptions} /><Route path="/payments" component={PaymentManagement} /><Route path="/payment-test" component={PaymentTest} /><Route path="/categories" component={Browse} />
-    <Route path="/publishers" component={() => <Browse />} /><Route path="/about" component={() => <InfoPage type="about" />} /><Route path="/contact" component={() => <InfoPage type="contact" />} />
-    <Route path="/privacy" component={() => <InfoPage type="privacy" />} /><Route path="/terms" component={() => <InfoPage type="terms" />} /><Route path="/copyright" component={() => <InfoPage type="copyright" />} /><Route path="/faq" component={FaqPage} />
-    <Route path="/publisher-signup" component={() => <InfoPage type="about" />} /><Route path="/library-signup" component={() => <InfoPage type="about" />} />
-    <Route component={NotFound} />
-  </Switch>;
-}
+function Router() { return <Switch>
+  <Route path="/" component={Home} /><Route path="/browse" component={Browse} /><Route path="/search" component={SearchBooks} /><Route path="/books/:slug" component={BookDetail} /><Route path="/books/:bookId/reviews" component={Reviews} /><Route path="/add-book" component={AddBook} />
+  <Route path="/authors/:userId" component={AuthorProfile} /><Route path="/publishers/:publisherId" component={PublisherProfile} /><Route path="/bookstores/:bookstoreId" component={BookstoreProfile} /><Route path="/profile" component={Profile} />
+  <Route path="/admin" component={AdminDashboard} /><Route path="/admin/reports" component={ReportsManagement} /><Route path="/dashboard" component={AdminDashboard} /><Route path="/orders" component={Orders} /><Route path="/advertisements" component={AdvertisementManagement} /><Route path="/featured-listings" component={FeaturedListingManagement} /><Route path="/subscriptions" component={Subscriptions} /><Route path="/payments" component={PaymentManagement} /><Route path="/payment-test" component={PaymentTest} /><Route path="/categories" component={Browse} /><Route path="/publishers" component={() => <Browse />} />
+  <Route path="/about" component={() => <InfoPage type="about" />} /><Route path="/contact" component={() => <InfoPage type="contact" />} /><Route path="/privacy" component={() => <InfoPage type="privacy" />} /><Route path="/terms" component={() => <InfoPage type="terms" />} /><Route path="/copyright" component={() => <InfoPage type="copyright" />} /><Route path="/faq" component={FaqPage} /><Route path="/publisher-signup" component={() => <InfoPage type="about" />} /><Route path="/library-signup" component={() => <InfoPage type="about" />} /><Route component={NotFound} />
+</Switch>; }
 
-export default function App() {
-  return <ErrorBoundary><ThemeProvider defaultTheme="light"><TooltipProvider><Toaster /><Router /></TooltipProvider></ThemeProvider></ErrorBoundary>;
-}
+export default function App() { return <ErrorBoundary><ThemeProvider defaultTheme="light"><TooltipProvider><Toaster /><Router /></TooltipProvider></ThemeProvider></ErrorBoundary>; }
